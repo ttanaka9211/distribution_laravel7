182d2c339b88de85757a28bc95df6b1ecfbf4fbd
ttanaka9211 <ttanaka@hatchdogs.net>
2020-11-10 23:07:22 +0900

    ログインリダイレクト

---
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class ChangePasswordController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function showChangePasswordForm()
+    {
+        return view('auth\passwords\change');
+    }
+
+    public function changePassword()
+    {
+        /* ===ここにパスワード変更の処理=== */
+    }
+}

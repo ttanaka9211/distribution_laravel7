4d47f8f70efd0ed7f22f4173f212dab6c05431f4
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-11-11 17:40:35 +0900

    パスワード変更

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -3,21 +3,31 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ChangePasswordRequest;
+use Illuminate\Support\Facades\Auth;
 
 class ChangePasswordController extends Controller
 {
-    /* public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
-    } */
+    }
 
     public function showChangePasswordForm()
     {
         return view('auth/passwords/change');
     }
 
-    public function changePassword()
+    public function changePassword(ChangePasswordRequest $request)
     {
-        /* ===ここにパスワード変更の処理=== */
+        /* ===ValidationはChangePasswordRequestで処理=== */
+
+        //パスワード変更処理
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('password'));
+        $user->save();
+
+        //パスワード変更処理後、homeにリダイレクト
+        return redirect()->route('home')->with('status', __('Your password has been changed.'));
     }
 }

fc7d79c8a1bf3016c0123e39cb98356cd176e0c7
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-11-16 12:34:02 +0900

    投稿の詳細・コメントの機能

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,4 @@ Route::post('/setting/email', 'SettingController@changeEmail')->name('email.chan
 
 //posts
 Route::get('/', 'PostsController@index')->name('top');
-Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);

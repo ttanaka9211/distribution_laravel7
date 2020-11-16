f7a0015d40e4cbcc1ccf6ddf8985a97fccb1f247
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-11-16 12:24:20 +0900

    投稿の作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
@@ -16,5 +17,22 @@ class PostsController extends Controller
         $posts = Post::orderBy('created_at', 'desc')->get();
 
         return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $params = $request->validate([
+            'title' => 'require|max:50',
+            'body' => 'required|max:2000',
+        ]);
+
+        Post::create($params);
+
+        return redirect()->route('top');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,4 @@ Route::post('/setting/email', 'SettingController@changeEmail')->name('email.chan
 
 //posts
 Route::get('/', 'PostsController@index')->name('top');
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);

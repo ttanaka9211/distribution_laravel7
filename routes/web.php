c7f95fd23a566ef9562d19ed037238222546a492
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-11-20 11:27:21 +0900

    ユーザー登録修正

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['verify' => true]);
 
-Route::get('/test', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/setting/password/change', 'Auth\ChangePasswordController@showChangePasswordForm')->name('password.form');
 Route::post('/setting/password/change', 'Auth\ChangePasswordController@ChangePassword')->name('password.change');
 Route::get('/setting/deactive', 'Auth\DeactiveController@showDeactiveForm')->name('deactive.form');

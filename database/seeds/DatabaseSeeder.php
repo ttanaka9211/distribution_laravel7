2725345acffeba63a34055e766c5a0cc77fb4134
ttanaka9211 <ttanaka@hatchdogs.net>
2020-11-20 21:30:18 +0900

    role設定

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         //factory(App\User::class, 10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(PostsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
